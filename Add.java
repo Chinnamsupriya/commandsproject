49c9634d8325b7140c572d384caf184a966563df
Chinnamsupriya <supriya.chinnam2000@gmail.com>
2024-07-11 16:43:44 +0530

    All files added successful

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Addition
+{
+   public static void main(String[] args)
+  {
+       int a=20;
+       int b=40;
+       int add=a+b;
+       System.out.println("Addition of two numbers="+add);
+  }
+}
---
+++ PosorNeg.java
@@ -0,0 +1,16 @@
+public class PosorNeg
+{
+   public static void main(String[] args)
+   {
+     int n;
+     System.out.println("Enter the given value of n");
+     if(n>1)
+     {
+        System.out.println("The given number is positive");
+     }
+     else
+     {
+        System.out.println("The given number is Negative");
+     }
+  }
+}
